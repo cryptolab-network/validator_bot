5de8ec90a2180a7f34f50c01d4e1260005228333
Yao-Hsin Chen <iisaintz@gmail.com>
2021-03-29 09:55:56 +0800

    add /start handler

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ const Notification = require('./notification');
     const polling = new Scheduler(chainData, db, notification);
     polling.start();
 
+    bot.onText(/\/start/, (msg, match) => {
+      const help = `
+        This bot helps you to monitor the nomination status of your validator.
+        /add - add a new validator
+        /list - list added validators
+        /remove - remove an existing validator
+        /trend - show nomination trend of your validators
+        /help - display this message
+      `;
+      bot.sendMessage(msg.chat.id, help);
+    })
+
     bot.onText(/\/help/, (msg, match) => {
       const help = `
         This bot helps you to monitor the nomination status of your validator.
