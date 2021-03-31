5c191938689cbdc857abb5a156f980948d1d6478
Yao-Hsin Chen <iisaintz@gmail.com>
2021-03-31 19:11:51 +0800

    retrive all validator data

--- src/ChainData.js
+++ src/ChainData.js
@@ -53,4 +53,94 @@ module.exports = class ChainData {
     return identity;
   }
 
+  getAllValidators = async (address) => {
+    const api = await this.handler.getApi();
+
+    let validators = [];
+    let intentions = [];
+
+    let [
+      validatorAddresses,
+      waitingInfo,
+      // nominators,
+    ] = await Promise.all([
+      api.query.session.validators(),
+      api.derive.staking.waitingInfo(),
+      // api.query.staking.nominators.entries(),
+    ])
+
+    validators = await Promise.all(
+      validatorAddresses.map((authorityId) =>
+        api.derive.staking.query(authorityId, {
+          withDestination: false,
+          withExposure: true,
+          withLedger: true,
+          withNominations: true,
+          withPrefs: true,
+        })
+      )
+    )
+    validators = await Promise.all(
+      validators.map((validator) =>
+        api.derive.accounts.info(validator.accountId).then(({ identity }) => {
+          identity.judgements = []; // remove it to avoid cache error
+          validator.rewardDestination = {}; // remove it to avoid cache error
+          return {
+            ...validator,
+            identity,
+            active: true,
+          }
+        })
+      )
+    )
+    intentions = await Promise.all(
+      JSON.parse(JSON.stringify(waitingInfo.info)).map((intention) =>
+        api.derive.accounts.info(intention.accountId).then(({ identity }) => {
+          identity.judgements = []; // remove it to avoid cache error
+          intention.rewardDestination = {}; // remove it to avoid cache error
+          return {
+            ...intention,
+            identity,
+            active: false,
+          }
+        })
+      )
+    )
+    // nominators = await Promise.all(
+    //   nominators.map((nominator) => 
+    //     api.derive.balances.all(nominator[0].toHuman()[0]).then((balance) => {
+    //       return {
+    //         ...nominator,
+    //         balance: {
+    //           lockedBalance: balance.lockedBalance,
+    //           freeBalance: balance.freeBalance
+    //         }
+    //       }
+    //     })
+    //   )
+    // )
+    // const nominations = nominators.map((nominator) => {
+    //   if(nominator[1] === null) {
+    //     return  {
+    //       nominator: nominator[1],
+    //       targets: [],
+    //       balance: null,
+    //     }
+    //   }
+    //   const accountId = nominator[0].toHuman()[0];
+    //   const targets = nominator[1].toHuman().targets;
+    //   const balance = JSON.parse(JSON.stringify(nominator.balance));
+    //   return {
+    //     accountId,
+    //     targets,
+    //     balance
+    //   }
+    // })
+    console.log(validators)
+    return {
+      validators: validators.concat(intentions),
+      // nominations
+    }
+  }
+
 }
